d850efea7186c4d7857f86e6624100eb8a41de9e
Dany Dodson <danydodson@gmail.com>
2023-02-28 08:53:41 -0600

    :sparkles: updates

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    // publicPath: path.resolve(__dirname, '../public'),
+    publicPath: path.resolve(__dirname, '../public'),
   },
   plugins: [
     new MiniCssExtractPlugin({
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Layout from './components/layout'
 import Hero from './components/hero'
 import Main from './components/main'
 
-
 const App = () => {
   return (
     <Layout>
