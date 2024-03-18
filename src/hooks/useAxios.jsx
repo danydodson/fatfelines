48bda48a0bd380ef6b2a99ff4de7d3d62d8bb7a3
danydodson <danydodson@gmail.com>
2024-03-18 08:04:10 -0500

    Refactor useAxios hook to include default headers and update proxy URL

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,32 +1,37 @@
-import React,{ useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const proxy = 'https://cors-through.herokuapp.com/'
+// const proxy = 'https://cors-through.herokuapp.com/'
+const proxy = 'https://cors-anywhere.herokuapp.com/'
 const url = 'https://www.shelterluv.com/api/v3/available-animals/11886?species=Cat'
 
 axios.defaults.baseURL = proxy + url
+axios.defaults.headers = {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+}
 
 const useAxios = axiosParams => {
-  const [response, setResponse] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState('')
+    const [response, setResponse] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
 
-  const fetchData = async (params) => {
-    try {
-      const response = await axios.request(params)
-      setResponse(response.data)
-    } catch (error) {
-      setError(error)
-    } finally {
-      setLoading(false)
+    const fetchData = async (params) => {
+        try {
+            const response = await axios.request(params)
+            setResponse(response.data)
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading(false)
+        }
     }
-  }
 
-  useEffect(() => {
-    fetchData(axiosParams)
-  }, [])
+    useEffect(() => {
+        fetchData(axiosParams)
+    }, [])
 
-  return { response, loading, error }
+    return { response, loading, error }
 }
 
 export default useAxios
