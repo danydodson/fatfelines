0e952437cfb4d0a44f859b2240fbb8fce97b32fb
danydodson <danydodson@gmail.com>
2024-04-13 03:02:26 -0500

    Update import path for Collection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Collection from './components/Collection'
+import Collection from './components/Collection.jsx'
 import Header from './components/Header'
 import Layout from './components/Layout'
 import Hero from './components/Hero'
